c07b97ca2d6374b08c63babb79e0db47aed10329
Raphael-PC\Raphael <wi117b112@technikum-wien.at>
2018-06-04 18:31:46 +0200

    kunde anlegen basic implementation

--- inc/kundeanlegen.inc.php
+++ inc/kundeanlegen.inc.php
@@ -1,2 +1,32 @@
 <h1>Kunden anlegen</h1>
-
+<form>
+  <div>
+  <label for="vorname">Vorname</label>
+    <input type="text" class="form-control" name="vorname" id="vorname" placeholder="Vorname">
+  </div>
+    <div>
+  <label for="nachname">Nachname</label>
+    <input type="text" class="form-control" name="nachname" id="nachname" placeholder="Nachname">
+  </div>
+  <div class="form-group">
+    <label for="email">Email Adresse</label>
+    <input type="email" class="form-control" id="email" placeholder="Email">
+  </div>
+  <div>
+  <label for="telefon">Telefon</label>
+    <input type="tel" class="form-control" name="telefon" id="telefon" placeholder="+43699 123 456 78 ">
+  </div>
+    <div>
+  <label for="strasse">Straße</label>
+  <input type="text" class="form-control" name="strasse" id="strasse" placeholder="Straße">
+  <div>
+  <label for="Hausenummer">Hausnummer</label>
+    <input type="text" class="form-control" name="hausnummer" id="hausnummer" placeholder="Hausnummer">
+  </div>
+  <div>
+  <label for="plz">PLZ</label>
+  <input type="number" class="form-control" name="plz" id="plz" placeholder="PLZ">
+  </div>
+  <br>
+  <button type="submit" class="btn btn-default">Kunde anlegen</button>
+</form>
