f6720354c9f68c8cbc83d8d2df9f20b8c55e0647
Raphael-PC\Raphael <wi117b112@technikum-wien.at>
2018-06-25 14:41:17 +0200

    name change of a field

--- inc/rechnunglegen.inc.php
+++ inc/rechnunglegen.inc.php
@@ -1,7 +1,7 @@
 <h1>Rechnung legen</h1>
 <form name="rechnungform" class="form-inline" action="index.php?section=rechnunglegen" method="post" onsubmit="popitup()">
   <div class="form-group">
-    <label for="auftragnummer">Auftragnummer</label>
-    <input type="text" class="form-control" name="auftragnummer" id="auftragnummer" placeholder="Auftragnummer">
+    <label for="auftragnummer">Bestellnummer</label>
+    <input type="text" class="form-control" name="auftragnummer" id="auftragnummer" placeholder="Bestellnummer">
   <button type="submit" class="btn btn-default">Rechnung erstellen</button>
 </form>
